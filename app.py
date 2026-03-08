965e120c85d68ac4e7dacb223a735f40598f0324
laodeazwaraiman <aimansrlaino@gmail.com>
2026-03-09 03:48:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-import os
-st.write("Daftar file yang terdeteksi di server:", os.listdir())
 
 # 1. Load Model dan Scaler
 model = joblib.load('diamond_best_model.pkl')
@@ -66,3 +64,4 @@ if st.button("Prediksi Harga"):
 
 
 st.info("Catatan: Prediksi didasarkan pada model Machine Learning yang telah dilatih.")
+
